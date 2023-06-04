7e9f8839d5d36f6950623dc8e51a031cb4bdd32f
Rohan27s <rohangotwal3@gmail.com>
2023-06-04 16:14:40 +0530

    edit

--- components/CardQuizset.jsx
+++ components/CardQuizset.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete'
+import DeleteIcon from '@mui/icons-material/Delete';
 import Link from 'next/link';
-const CardQuizset = ({quiz}) => {
-  console.log(quiz)
+import axios from 'axios';
+
+const CardQuizset = ({ quiz }) => {
+  const handleDelete = async () => {
+    const shouldDelete = window.confirm('Are you sure you want to delete this quiz?');
+    if (shouldDelete) {
+      try {
+        await axios.delete(`https://mental-health-backend.vercel.app/api/quizzies/${quiz._id}`);
+        console.log('Quiz deleted successfully');
+        window.location.reload(); 
+        // Perform any additional actions after successful deletion
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    }
+  };
+
   return (
     <>
-    <div class="card">
-    <div class="card-edit">
-    <Link href={`/quizset/${quiz._id}`}><EditIcon/></Link>
-    
-    </div>
-  {/* <img src="card-image.jpg" alt="Card Image" class="card-image"/> */}
-  <div class="card-content">
-    <h2 class="card-title">{quiz.title}</h2>
-    <p class="card-text">{quiz.desc}</p>
-    <p class="card-text">TotalQuestions : {quiz.quiz.length}</p>
-    <p style={{background:'red'}} href="#" class="card-button">Delete</p>
-  </div>
-</div>
-</>
-  )
-}
+      <div className="card">
+        <div className="card-edit">
+          <Link href={`/quizset/${quiz._id}`}>
+            <EditIcon />
+          </Link>
+        </div>
+        {/* <img src="card-image.jpg" alt="Card Image" className="card-image"/> */}
+        <div className="card-content">
+          <h2 className="card-title">{quiz.title}</h2>
+          <p className="card-text">{quiz.desc}</p>
+          <p className="card-text">Total Questions: {quiz.quiz.length}</p>
+          <p style={{ background: 'red',cursor:'pointer' }} onClick={handleDelete} className="card-button">
+            Delete
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CardQuizset
+export default CardQuizset;
