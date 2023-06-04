68e8373107825e4829a093f7bfb39023a687700a
Rohan27s <rohangotwal3@gmail.com>
2023-06-04 11:46:21 +0530

    edit

--- pages/quizset/[_id].js
+++ pages/quizset/[_id].js
@@ -1,8 +1,11 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-const Quizsetdetails = ({quiz}) => {
-  console.log(quiz)
-  const [quizset, setQuizset] = useState([{ ques: '' }]);
+import React, { useState } from 'react';
+import axios from 'axios';
+// import './Quizsetdetails.css'; // Import the CSS file
+
+const Quizsetdetails = ({ quiz }) => {
+  console.log(quiz);
+  const [quizset, setQuizset] = useState([...quiz.quiz]);
+  const [isEditMode, setIsEditMode] = useState(false); // Track whether it's in edit mode or not
 
   const handleChange = (e, index) => {
     const { value } = e.target;
@@ -20,42 +23,54 @@ const Quizsetdetails = ({quiz}) => {
     updatedQuiz.splice(index, 1);
     setQuizset(updatedQuiz);
   };
-  return (
-  <>
-    <div><h1>{quiz.title}</h1></div>
-    <p>{quiz.desc}</p>
-    <h3>Total Number of Questions : {quiz.quiz.length}</h3>
-    <div>
-    {quiz.quiz.map((item, index) => (
-        <div key={index}>
-          <input
-            type="text"
-            value={item.ques}
-            onChange={(e) => handleChange(e, index)}
-          />
-          {index === quiz.length - 1 && (
-            <button onClick={handleAddField}>Add Field</button>
-          )}
-          {index !== quiz.length - 1 && (
-            <button onClick={() => handleRemoveField(item._id)}>Remove Field</button>
-          )}
-        </div>
-      ))}
 
-    </div>
+  const handleUpdateDetails = () => {
+    // Implement your logic to update the details using an API call
+    console.log('Updating details...');
+  };
+
+  return (
+    <>
+      <div>
+        <h1>{quiz.title}</h1>
+      </div>
+      <p>{quiz.desc}</p>
+      <h3>Total Number of Questions: {quizset.length}</h3>
+      <div className="quizset-form"> {/* Add a class name for the form */}
+        {quizset.map((item, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              value={item.ques}
+              onChange={(e) => handleChange(e, index)}
+              disabled={!isEditMode} // Disable input fields if not in edit mode
+            />
+            {index === quizset.length - 1 ? (
+              <button onClick={handleAddField}>Add Field</button>
+            ) : (
+              <button onClick={() => handleRemoveField(index)}>Remove Field</button>
+            )}
+          </div>
+        ))}
+        {isEditMode ? (
+          <button onClick={handleUpdateDetails}>Update Details</button> 
+        ) : (
+          <button onClick={() => setIsEditMode(true)}>Edit</button> 
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
     `https://mental-health-backend.vercel.app/api/quizzies/${params._id}`
   );
-  // console.log(params._id)
   return {
     props: {
       quiz: res.data,
     },
   };
-}
-export default Quizsetdetails
+};
+
+export default Quizsetdetails;
