72762a3108e91db24c108d5fc4dcd25ff2a3ae20
Rohan27s <rohangotwal3@gmail.com>
2023-06-04 16:17:17 +0530

    edit

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import Nav from '../components/Nav'
+
 export default function Home() {
   return (
     <div className='welc'>
-    <h1>Welcome to<br/>Mental Health Dashboard</h1>
+      <Head>
+        <title>Admin | Mental Health</title>
+      </Head>
+      <h1>Welcome to<br/>Mental Health Dashboard</h1>
     </div>
   )
 }
