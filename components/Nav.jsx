2c5aac3d066b2eac24b8f16651f1e9107483ffd1
Rohan27s <rohangotwal3@gmail.com>
2023-06-04 19:21:14 +0530

    edit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 const Nav = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Update the value of isLoggedIn in localStorage to false
+    localStorage.setItem('isLoggedIn', 'false');
+
+    // Redirect the user to the login page
+    router.push('/login');
+  };
+
   return (
     <div className='navbar'>
-    <nav style={{display:'flex', flexDirection:'column'}}>
-        <Link href={'/'} className='lis'>Dashboard</Link>
-        <Link href={'/quizset'} className='lis'>QuizSet</Link>
-        <Link href={'/users'} className='lis'>Users</Link>
-        <Link href={'/'} className='lis'>Logout</Link>
-    </nav>
+      <nav style={{ display: 'flex', flexDirection: 'column' }}>
+        <Link href={'/'} className='lis'>
+          Dashboard
+        </Link>
+        <Link href={'/quizset'} className='lis'>
+          QuizSet
+        </Link>
+        <Link href={'/users'} className='lis'>
+          Users
+        </Link>
+        <button className='lis' onClick={handleLogout}>
+          Logout
+        </button>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
